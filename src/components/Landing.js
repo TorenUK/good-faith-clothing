9b30f1e0e32a777d19822b7a02deabe029d9dc0a
TorenUK <td_fraser@hotmail.co.uk>
2020-10-02 11:10:00 +0100

    initial commit

---
+++ src/components/Landing.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// components
+import "./Landing.css";
+
+function Landing() {
+  return (
+    <div className="landing">
+      <div className="landing__logo">
+        <img src="gfc.logo.svg" />
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
